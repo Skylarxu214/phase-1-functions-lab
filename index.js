e7ae6b8bc756eb46593d96e1ffcd6eeca824b9fc
Tian Xu <xueqingxu0302@gmail.com>
2021-11-23 10:12:39 -0700

    commit tree 4fc2ae9eeb5b54cabffa618499659c4dffd278e3

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(n){
+    return Math.abs(n-42)
+}
+function distanceFromHqInFeet(n){
+    return Math.abs(n-42)*264
+}
+function distanceTravelledInFeet(n,n1){
+    return Math.abs(n-n1)*264
+}
+function calculatesFarePrice(start, destination){
+    if (Math.abs(start - destination) *264 < 400){
+        return 0
+    }
+   
+
+    else if (Math.abs(start - destination) *264 > 2500 ){
+    return 'cannot travel that far'
+    }
+    else if (Math.abs(start - destination) *264 > 2000){
+        return 25;
+    }
+    else if (Math.abs(start - destination) *264  <= 2000){
+        return (Math.abs(start - destination) *264 - 400) * 0.02
+    }
+
+}
